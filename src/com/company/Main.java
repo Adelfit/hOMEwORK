9b34d4c6ce3466eea8ff6051605eb766adad83c5
Adelfit <adelfitnesstrain@gmail.com>
2020-10-17 16:21:57 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String privet = "Привет, ";
+        String mir = "Мир!";
+        String privetMir = privet + mir;
+        System.out.println(privetMir);
+    }
+}
